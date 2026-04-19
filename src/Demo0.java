7c2ab286eafcf46b32ddbbcf9131ebd4750917c2
Titanium_awa <196439259+Titanium-awa@users.noreply.github.com>
2026-04-19 21:09:38 +0800

    1

---
+++ src/Demo0.java
@@ -0,0 +1,23 @@
+public class Demo0 {
+    public static void main(String[] args) {
+        int num = 7;
+        // double num1 = 1e308;
+        String str = "awa";
+
+        System.out.println(num);
+        System.out.println(str);
+
+        System.out.println(0.1 + 0.2);
+        System.out.println(0.1f + 0.2f);
+
+        /*
+        System.out.println("awa" == "awa");
+        System.out.println("awa".equals("awa"));
+        System.out.println(new String("awa") == new String("awa"));
+        System.out.println(new String("awa").equals(new String("awa")));
+         */
+
+        // if (1) {}
+        // if (true) {}
+    }
+}

fb0a8c6353fdafc15f7ed753788052298fb07e56
Titanium_awa <196439259+Titanium-awa@users.noreply.github.com>
2026-05-01 12:06:34 +0800

    1

--- src/structure/Demo6.java
+++ src/structure/Demo6.java
@@ -2,6 +2,20 @@ package structure;
 
 public class Demo6 {
     public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + "*" + j + "=" + i * j + "\t");
+            }
+            System.out.print("\n");
+        }
 
+        System.out.println("====================");
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= (10 - i); j++) {
+                System.out.print(i + "*" + j + "=" + i * j + "\t");
+            }
+            System.out.print("\n");
+        }
     }
 }
